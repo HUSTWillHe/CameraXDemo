590af8d048b725eed20734f1fe06855a0beff4a0
heliw <heliwei1994@gmail.com>
2025-01-31 17:23:03 +0800

    复制标准答案版本，修改相机id之后能在模拟器上跑通。

--- app/src/main/java/com/example/cameraxdemo/MainActivity.kt
+++ app/src/main/java/com/example/cameraxdemo/MainActivity.kt
@@ -1,58 +1,125 @@
 package com.example.cameraxdemo
 
-import android.content.pm.PackageManager
-import android.os.Build
-import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.example.cameraxdemo.databinding.ActivityMainBinding
 import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
 import android.opengl.GLSurfaceView
+import android.os.Build
+import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
+import android.util.Log
 import android.util.Size
+import android.view.OrientationEventListener
 import android.view.Surface
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import com.example.cameraxdemo.databinding.ActivityMainBinding
 import com.example.glsurfaceviewdemo.MyGLRenderer
 import com.example.glsurfaceviewdemo.MyGLSurfaceView
-import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var viewBinding: ActivityMainBinding
-    private lateinit var mCamera: CameraDevice
-    private lateinit var mCamtureSession: CameraCaptureSession
+    private var mCamera: CameraDevice? = null
+    private var mCaptureSession: CameraCaptureSession? = null
+
     private var mHandler: Handler? = null
     private var mSurfaceTexture: SurfaceTexture? = null
-    private var mRenderer: MyGLRenderer? = null
+
     private var mGLView: MyGLSurfaceView? = null
-    private var mPreviewSize: Size = Size(0, 0)
+    private var mRender: MyGLRenderer? = null
+
+    private var mPreviewSize: Size = Size(0,0)
+
+    private lateinit var mOrientationEventListener: MyOrientationEventListener
+    private var mSensorOrientation = 0
+    private var mPhoneOrientation = 0
+
+    class MyOrientationEventListener(context: Context): OrientationEventListener(context){
+        private var activity: MainActivity? = null
+        override fun onOrientationChanged(orientation: Int) {
+//            Log.e("Orientation:", "$orientation")
+            if(activity != null) {
+                val rotation = activity?.getRotation()
+                activity?.setPhoneOrietation(rotation!!)
+                activity?.calculatePreviewRotation()
+            }
+        }
+
+        fun setActivity(act: MainActivity){
+            this.activity = act
+        }
+    }
+
+    fun getRotation(): Int {
+        val display = (getSystemService(WINDOW_SERVICE) as WindowManager).defaultDisplay
+        val screenRotation = display.rotation
+
+        when(screenRotation){
+            Surface.ROTATION_0 -> {
+                return 0
+            }
+            Surface.ROTATION_90 -> {
+                return 90
+            }
+            Surface.ROTATION_180 -> {
+                return 180
+            }
+            Surface.ROTATION_270 -> {
+                return 270
+            }
+            else -> {
+                return 0
+            }
+        }
+    }
+
+    fun setPhoneOrietation(orientation: Int){
+        this.mPhoneOrientation = orientation
+    }
+
+    fun startOrientationChangeListener() {
+        mOrientationEventListener = MyOrientationEventListener(this)
+        mOrientationEventListener.setActivity(this)
+        mOrientationEventListener.enable()
+    }
+
+    fun calculatePreviewRotation(){
+        val result = ( mSensorOrientation - mPhoneOrientation * -1 + 360 ) % 360
+        //Log.e("calculatePreviewRotation:", "$result")
+        mRender?.setOrientation(result)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (mGLView == null) {
+
+        if(mGLView == null) {
             mGLView = MyGLSurfaceView(this)
-
             setContentView(mGLView)
-
-            mRenderer = mGLView?.getRender()
-
             mGLView?.renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
+
+            mRender = mGLView?.getRender()
         }
 
-        if (mHandler == null) {
-            mHandler = object : Handler(Looper.getMainLooper()) {
+        startOrientationChangeListener()
+
+        if(mHandler == null) {
+            mHandler = object: Handler(Looper.getMainLooper()) {
                 override fun handleMessage(msg: Message) {
                     super.handleMessage(msg)
-                    when (msg.what) {
+                    when(msg.what){
                         1 -> {
                             val texture = msg.obj as SurfaceTexture
                             mSurfaceTexture = texture
@@ -61,18 +128,38 @@ class MainActivity : AppCompatActivity() {
                         }
 
                         else -> {
-                            Log.w(TAG, "othres message: ${msg.what}")
+                            Log.w(TAG, "others message: ${msg.what}")
                         }
                     }
+
                 }
             }
         }
 
-        if (allPermissionGranted()) {
+        //Request Camera permissions
+        if(allPermissionGranted()) {
             startCamera()
         } else {
-            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSION)
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUIRED_CODE_PERMISSIONS)
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        mRender?.setHandler(null)
+
+        mGLView?.onPause()
+        mCaptureSession?.close()
+        mCaptureSession = null
+
+        mCamera?.close()
+        mCamera = null
+    }
+
+    //代码中的all相当于一个for循环
+    private fun allPermissionGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
     override fun onRequestPermissionsResult(
@@ -81,104 +168,120 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSION) {
-            if (allPermissionGranted()) {
+        if (requestCode == REQUIRED_CODE_PERMISSIONS) {
+            if(allPermissionGranted()) {
                 startCamera()
             } else {
-                Toast.makeText(this, "Permission not granted.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
                 finish()
             }
         }
     }
 
     private fun captureVideo() {
-        if (mCamera == null) return
-        if (mSurfaceTexture == null) return
 
-        Log.d(TAG, "set default buffer size: ${mPreviewSize.width}   ${mPreviewSize.height}")
+        if(mCamera == null) return
+        if(mSurfaceTexture  == null) return
+
         mSurfaceTexture?.setDefaultBufferSize(mPreviewSize.width, mPreviewSize.height)
         val surface = Surface(mSurfaceTexture)
 
-        val requestBuilder = mCamera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-        requestBuilder.addTarget(surface)
 
-        val output = listOf(surface)
-        mCamera.createCaptureSession(output, object : CameraCaptureSession.StateCallback() {
+        val requestBuilder = mCamera?.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+        requestBuilder?.addTarget(surface)
+
+        val outputs = listOf(surface)
+        mCamera?.createCaptureSession(outputs, object: CameraCaptureSession.StateCallback(){
             override fun onConfigured(session: CameraCaptureSession) {
-                mCamtureSession = session
+                //TODO("Not yet implemented")
+                mCaptureSession= session
+                //capture
                 session.setRepeatingRequest(requestBuilder!!.build(), null, null)
             }
 
-            override fun onConfigureFailed(session: CameraCaptureSession) {
+            override fun onConfigureFailed(p0: CameraCaptureSession) {
                 //TODO("Not yet implemented")
             }
         }, null)
+
+
     }
 
-    fun chooseBestSize(sizes: Array<Size>, width: Int, height: Int): Size {
+    fun chooseBestSize(sizes: Array<Size>, width: Int, height: Int): Size{
         var bestSize: Size = sizes[0]
 
         var minDiffWidth = Int.MAX_VALUE
         var minDiffHeight = Int.MAX_VALUE
 
-        for (size in sizes) {
-            val diffWidth = abs(size.width - width)
-            val diffHeight = abs(size.height - height)
+        val targetWidth = width
+        val targetHeight = height
 
+        for (size in sizes) {
+            val diffWidth = Math.abs(size.width - targetWidth)
+            val diffHeight = Math.abs(size.height - targetHeight)
+
+            // 检查分辨率的宽高比例是否是4:3或16:9
             val isAspectRatioValid =
-                abs(size.width.toFloat() / size.height - 4.0f / 3.0f) < 0.01f ||
-                        abs(size.width.toFloat() / size.height - 16.0f / 9.0f) < 0.01f
-            if (isAspectRatioValid && (diffWidth < minDiffWidth || (diffWidth == minDiffWidth && diffHeight < minDiffHeight))) {
+                Math.abs(size.width.toFloat() / size.height - 4.0f / 3.0f) < 0.01f ||
+                        Math.abs(size.width.toFloat() / size.height - 16.0f / 9.0f) < 0.01f
+
+            if (isAspectRatioValid && (diffWidth < minDiffWidth ||
+                        (diffWidth == minDiffWidth && diffHeight < minDiffHeight))) {
                 bestSize = size
-                minDiffHeight = diffHeight
                 minDiffWidth = diffWidth
+                minDiffHeight = diffHeight
             }
         }
+
         return bestSize
     }
 
     private fun startCamera() {
+
         val manager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
-        val cameraId = manager.cameraIdList[0]
+
+        // todo: 改回来
+        var cameraId = manager.cameraIdList[1]
         val characteristics = manager.getCameraCharacteristics(cameraId)
+        mSensorOrientation = characteristics[CameraCharacteristics.SENSOR_ORIENTATION]!!
+//        Log.e("startCamera:", "$mSensorOrientation")
+        calculatePreviewRotation()
+
         val map = characteristics[CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP]!!
+
         mPreviewSize = chooseBestSize(map.getOutputSizes(SurfaceTexture::class.java), 1280, 720)
-        Log.d(TAG, "best size: ${mPreviewSize.width},  ${mPreviewSize.height}")
 
         try {
             manager.openCamera(cameraId, object : CameraDevice.StateCallback() {
                 override fun onOpened(camera: CameraDevice) {
+                    //...
                     mCamera = camera
-                    mRenderer?.setHandler(mHandler)
+                    mRender?.setHandler(mHandler)
+                    mRender?.setVideoSize(mPreviewSize)
                 }
 
-                override fun onDisconnected(camera: CameraDevice) {
-//                    TODO("Not yet implemented")
+                override fun onDisconnected(p0: CameraDevice) {
+                    //TODO("Not yet implemented")
                 }
 
-                override fun onError(camera: CameraDevice, error: Int) {
-//                    TODO("Not yet implemented")
+                override fun onError(p0: CameraDevice, p1: Int) {
+                    //TODO("Not yet implemented")
                 }
             }, null)
-        } catch (se: SecurityException) {
+        }catch (se: SecurityException){
             Log.e(TAG, se.toString())
         }
     }
 
-    private fun allPermissionGranted(): Boolean {
-        return REQUIRED_PERMISSIONS.all { permission ->
-            ContextCompat.checkSelfPermission(
-                baseContext,
-                permission
-            ) == PackageManager.PERMISSION_GRANTED
-        }
-    }
-
-
+    //伴生对象
+    //相当于Java中的静态成员,静态方法
     companion object {
-        private const val TAG = "CameraXDemo"
+        private const val TAG = "CameraXApp"
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-        private const val REQUEST_CODE_PERMISSION = 10
+        private const val REQUIRED_CODE_PERMISSIONS = 10
+
         private val REQUIRED_PERMISSIONS = mutableListOf(
             Manifest.permission.CAMERA,
             Manifest.permission.RECORD_AUDIO
--- app/src/main/java/com/example/cameraxdemo/TextureRenderer.kt
+++ app/src/main/java/com/example/cameraxdemo/TextureRenderer.kt
@@ -2,146 +2,80 @@ package com.example.glsurfaceviewdemo
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.opengl.GLES11Ext
 import android.opengl.GLES30
-import android.opengl.GLUtils
-import android.opengl.Matrix
 import android.util.Log
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.FloatBuffer
 
-class TextureRenderer {
-//    private lateinit var mContext: Context
-
+class TextureRender {
+    //创建shader，并为其指定源码
     private val vertexShaderCode =
-        "uniform mat4 uTMatrix;" +
-                "attribute vec4 aPosition;" +
-                "attribute vec2 aTexCoord;" +
+        "attribute vec4 aPosition;" +
+                "uniform mat4 uMVPMatrix;" +
+                "uniform mat4 uSTMatrix;" +
+                "attribute vec4 aTexCoord;" +
                 "varying vec2 vTexCoord;" +
                 "void main() {" +
-                "  gl_Position = uTMatrix * aPosition;" +
-                "  vTexCoord = aTexCoord;" +
+                "  gl_Position = uMVPMatrix * aPosition;" +
+                "  vTexCoord = (aTexCoord).xy;" +
                 "}"
 
     private val fragmentShaderCode =
         "precision mediump float;" +
-                "#extension GL_OES_EGL_image_external : require \n" +
-                "uniform samplerExternalOES uSampler;" +
+        "#extension GL_OES_EGL_image_external : require \n" +
+                "uniform samplerExternalOES uTexture;" +
                 "varying vec2 vTexCoord;" +
                 "void main() {" +
-                "  gl_FragColor = texture2D(uSampler, vTexCoord);" +
-//                "  gl_FragColor = vec4(0.1, 1.0, 0.4, 1.0);" +
+//                "  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);" +
+                "  gl_FragColor = texture2D(uTexture, vTexCoord);" +
                 "}"
 
-    private val coordData = floatArrayOf(
+    /*private val vertexData = floatArrayOf(
+        -1.0f, 1.0f, 0.0f,  0.0f, 0.0f, //左上角
+        -1.0f, -1.0f, 0.0f, 0.0f, 1.0f, //左下角
+        1.0f, 1.0f, 0.0f,   1.0f, 0.0f, //右上角
+        1.0f, -1.0f, 0.0f,  1.0f, 1.0f  //右下角
+    )*/
+
+    private val vertexData = floatArrayOf(
         -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,  // 0 bottom left
         1.0f, -1.0f, 0.0f, 1.0f, 0.0f,   // 1 bottom right
         -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,  // 2 top left
         1.0f,  1.0f, 0.0f, 1.0f, 1.0f    // 3 top right
     )
-    private var translateMatrix = FloatArray(16)
-    private val color = floatArrayOf(0.5f, 0.5f, 0.5f, 1.0f)
-
-    private var mProgram = 0
-    private var mPositionHandle = 0
-    private var mTexCoordHandle = 0
-    private var mColorHandle = 0
-    private var mTMatrixHandle = 0
-    private var mSamplerHandle = 0
-
-    private var mTextureId = 0
 
     private var vboId = IntArray(1)
-    private var vaoId = IntArray(1)
+    private var vboTextureId = IntArray(1)
 
-    private fun loadShader(type: Int, shaderCode: String): Int {
-        val shader = GLES30.glCreateShader(type)
-        GLES30.glShaderSource(shader, shaderCode)
-        GLES30.glCompileShader(shader)
-        val vertexShaderCompiled = IntArray(1)
-        GLES30.glGetShaderiv(shader, GLES30.GL_COMPILE_STATUS, vertexShaderCompiled, 0)
-        if (vertexShaderCompiled[0] == GLES30.GL_FALSE) {
-            val log = GLES30.glGetShaderInfoLog(shader)
-            Log.e("OpenGL", "type $type  Shader Compilation Failed: $log")
-        }
-        return shader
-    }
+    private var mProgram = -1
+    private var mPositionHandle = -1
+    private var mTextureHandle = -1
+    private var mSamplerHandle = -1
+    private var mMVPMatrixHandle = -1
+    private var mSTMatrixHandle = -1
 
-//    private fun loadTexture(resourceId: Int): Int {
-//        val textureHandler = IntArray(1)
-//        GLES30.glGenTextures(1, textureHandler, 0)
-//        val texture = textureHandler[0]
-//
-//        GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, texture)
-//        GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_MIN_FILTER, GLES30.GL_LINEAR)
-//        GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_MAG_FILTER, GLES30.GL_LINEAR)
-//
-//        GLES30.glTexParameteri(
-//            GLES30.GL_TEXTURE_2D,
-//            GLES30.GL_TEXTURE_WRAP_S,
-//            GLES30.GL_CLAMP_TO_EDGE
-//        )
-//        GLES30.glTexParameteri(
-//            GLES30.GL_TEXTURE_2D,
-//            GLES30.GL_TEXTURE_WRAP_T,
-//            GLES30.GL_CLAMP_TO_EDGE
-//        )
-//
-//        val bitmap = BitmapFactory.decodeResource(mContext.resources, resourceId)
-//        GLUtils.texImage2D(GLES30.GL_TEXTURE_2D, 0, GLES30.GL_RGBA, bitmap, 0)
-//        bitmap.recycle()
-//
-//        return texture
-//    }
+    private var mTextureId = -1
 
-    private fun createTexture(): Int {
-        val textureIds = IntArray(1)
-        GLES30.glGenTextures(1, textureIds, 0)
-        GLES30.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textureIds[0])
-        GLES30.glTexParameteri(
-            GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
-            GLES30.GL_TEXTURE_MIN_FILTER,
-            GLES30.GL_LINEAR
-        )
-        GLES30.glTexParameteri(
-            GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
-            GLES30.GL_TEXTURE_MAG_FILTER,
-            GLES30.GL_LINEAR
-        )
+    private var vertexBuffer: FloatBuffer = ByteBuffer.allocateDirect(vertexData.size * 4)
+        .order(ByteOrder.nativeOrder()).asFloatBuffer()
+    /*
+    private var textureBuffer: FloatBuffer = ByteBuffer.allocateDirect(textureData.size * 4)
+        .order(ByteOrder.nativeOrder()).asFloatBuffer()
+    */
 
-        GLES30.glTexParameteri(
-            GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
-            GLES30.GL_TEXTURE_WRAP_S,
-            GLES30.GL_CLAMP_TO_EDGE
-        )
-        GLES30.glTexParameteri(
-            GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
-            GLES30.GL_TEXTURE_WRAP_T,
-            GLES30.GL_CLAMP_TO_EDGE
-        )
 
-        GLES30.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, 0)
-
-        return textureIds[0]
-    }
-
-    private lateinit var coordBuffer: FloatBuffer
-
-    fun getTextureId(): Int {
-        return mTextureId
-    }
 
     constructor() {
-//        mContext = context
+
+        vertexBuffer.put(vertexData).position(0)
+        //textureBuffer.put(textureData).position(0)
+
         mTextureId = createTexture()
 
-        val bb = ByteBuffer.allocateDirect(coordData.size * 4)
-        bb.order(ByteOrder.nativeOrder())
-        coordBuffer = bb.asFloatBuffer()
-        coordBuffer.put(coordData)
-        coordBuffer.position(0)
+        //Matrix.setIdentityM(translateMatrix, 0)
+        //Matrix.scaleM(translateMatrix, 0, 0.5f, 0.5f, 1.0f)
 
         val vertexShader = loadShader(GLES30.GL_VERTEX_SHADER, vertexShaderCode)
         val fragmentShader = loadShader(GLES30.GL_FRAGMENT_SHADER, fragmentShaderCode)
@@ -149,8 +83,8 @@ class TextureRenderer {
         mProgram = GLES30.glCreateProgram()
         GLES30.glAttachShader(mProgram, vertexShader)
         GLES30.glAttachShader(mProgram, fragmentShader)
-
         GLES30.glLinkProgram(mProgram)
+
         val linkStatus = IntArray(1)
         GLES30.glGetProgramiv(mProgram, GLES30.GL_LINK_STATUS, linkStatus, 0)
         if (linkStatus[0] == GLES30.GL_FALSE) {
@@ -158,68 +92,91 @@ class TextureRenderer {
             Log.e("OpenGL", "Program linking failed: $log")
         }
 
-        GLES30.glDeleteShader(vertexShader)
-        GLES30.glDeleteShader(fragmentShader)
+        GLES30.glGenBuffers(1, vboId, 0)
+        GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, vboId[0])
+        GLES30.glBufferData(GLES30.GL_ARRAY_BUFFER, vertexBuffer.capacity()*4, vertexBuffer, GLES30.GL_STATIC_DRAW)
 
-        GLES30.glGenVertexArrays(1, vaoId, 0)
-        GLES30.glBindVertexArray(vaoId[0])
-
-        Matrix.setIdentityM(translateMatrix, 0)
+        /*
+        GLES30.glGenBuffers(1, vboTextureId, 0)
+        GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, vboTextureId[0])
+        GLES30.glBufferData(GLES30.GL_ARRAY_BUFFER, textureBuffer.capacity()*4, textureBuffer, GLES30.GL_STATIC_DRAW)
+        */
 
         mPositionHandle = GLES30.glGetAttribLocation(mProgram, "aPosition")
         GLES30.glEnableVertexAttribArray(mPositionHandle)
 
-        mTexCoordHandle = GLES30.glGetAttribLocation(mProgram, "aTexCoord")
-        GLES30.glEnableVertexAttribArray(mTexCoordHandle)
+        mTextureHandle = GLES30.glGetAttribLocation(mProgram, "aTexCoord")
+        GLES30.glEnableVertexAttribArray(mTextureHandle)
 
-        mTMatrixHandle = GLES30.glGetUniformLocation(mProgram, "uTMatrix")
-        mSamplerHandle = GLES30.glGetUniformLocation(mProgram, "uSampler")
+        mSamplerHandle = GLES30.glGetUniformLocation(mProgram, "uTexture")
+        mMVPMatrixHandle = GLES30.glGetUniformLocation(mProgram, "uMVPMatrix")
+        mSTMatrixHandle = GLES30.glGetUniformLocation(mProgram, "uSTMatrix")
 
-        GLES30.glGenBuffers(1, vboId, 0)
-        GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, vboId[0])
-        GLES30.glBufferData(GLES30.GL_ARRAY_BUFFER, bb.capacity(), bb, GLES30.GL_STATIC_DRAW)
-
-        GLES30.glVertexAttribPointer(
-            mPositionHandle,
-            3,
-            GLES30.GL_FLOAT,
-            false,
-            5 * Float.SIZE_BYTES,
-            0
-        )
-        GLES30.glVertexAttribPointer(
-            mTexCoordHandle,
-            2,
-            GLES30.GL_FLOAT,
-            false,
-            5 * Float.SIZE_BYTES,
-            3 * Float.SIZE_BYTES
-        )
-
-//        mTextureId = loadTexture(R.drawable.icon)
         GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, 0)
-        GLES30.glBindVertexArray(0)
     }
 
-    fun draw() {
-        GLES30.glUseProgram(mProgram)
-        GLES30.glBindVertexArray(vaoId[0])
+    fun getTextureId(): Int {
+        return mTextureId;
+    }
 
-        GLES30.glUniformMatrix4fv(mTMatrixHandle, 1, false, translateMatrix, 0)
+    private fun loadShader(type: Int, shaderCode: String): Int {
+        val shader = GLES30.glCreateShader(type)
+        GLES30.glShaderSource(shader, shaderCode)
+        GLES30.glCompileShader(shader)
+
+        val vertexShaderCompiled = IntArray(1)
+        GLES30.glGetShaderiv(shader, GLES30.GL_COMPILE_STATUS, vertexShaderCompiled, 0)
+        if (vertexShaderCompiled[0] == GLES30.GL_FALSE) {
+            val log = GLES30.glGetShaderInfoLog(shader)
+            Log.e("OpenGL", "type $type  Shader Compilation Failed: $log")
+        }
+
+        return shader
+    }
+
+    private fun createTexture(): Int {
+
+        val textureIds = IntArray(1)
+        GLES30.glGenTextures(1, textureIds, 0)
+
+        GLES30.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textureIds[0])
+
+
+        GLES30.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES30.GL_TEXTURE_MIN_FILTER,
+            GLES30.GL_LINEAR.toFloat());
+        GLES30.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES30.GL_TEXTURE_MAG_FILTER,
+            GLES30.GL_LINEAR.toFloat());
+        GLES30.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES30.GL_TEXTURE_WRAP_S,
+            GLES30.GL_CLAMP_TO_EDGE);
+        GLES30.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES30.GL_TEXTURE_WRAP_T,
+            GLES30.GL_CLAMP_TO_EDGE);
+        GLES30.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, 0)
+
+        return textureIds[0]
+    }
+
+    fun draw(mvpMatrix: FloatArray, stMaxtrix: FloatArray) {
+        //使用program
+        GLES30.glUseProgram(mProgram)
+
+        //将数据传递给shader
+        GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, vboId[0])
+        GLES30.glVertexAttribPointer(mPositionHandle, 3, GLES30.GL_FLOAT, false, 5*Float.SIZE_BYTES, 0)
+
+        //GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, vboTextureId[0])
+        GLES30.glVertexAttribPointer(mTextureHandle, 2, GLES30.GL_FLOAT, false, 5*Float.SIZE_BYTES, 3*Float.SIZE_BYTES)
 
         GLES30.glActiveTexture(GLES30.GL_TEXTURE0)
-        GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, mTextureId)
+        GLES30.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, mTextureId)
         GLES30.glUniform1i(mSamplerHandle, 0)
 
+        GLES30.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0)
+        GLES30.glUniformMatrix4fv(mSTMatrixHandle, 1, false, stMaxtrix, 0)
+
+        //drawArray, 绘制三角形
         GLES30.glDrawArrays(GLES30.GL_TRIANGLE_STRIP, 0, 4)
 
-        GLES30.glBindVertexArray(0)
-    }
-
-    fun onDestroy() {
-        GLES30.glDeleteBuffers(1, vboId, 0)
-        GLES30.glDeleteVertexArrays(1, vaoId, 0)
-
-        GLES30.glDeleteProgram(mProgram)
+        GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, 0)
+        GLES30.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, 0)
     }
 }
